41a537470771fb2c6b87eaabefc51b3123105b42
yp945 <pan107104@outlook.com>
2023-12-25 17:08:33 +0800

    add system context variable.

--- assembler/src/test_data_generator.rs
+++ assembler/src/test_data_generator.rs
@@ -270,6 +270,11 @@ mod tests {
         generate_from_file("NonceHolder_asm.json".to_string(), "NonceHolder.json".to_string());
     }
 
+    #[test]
+    fn generate_system_context() {
+        generate_from_file("system_context_asm.json".to_string(), "system_context.json".to_string());
+    }
+
 
 
     fn generate_from_file(input_file_name: String, output_file_name: String) {
